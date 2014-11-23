bf84fa9f7d61d6a0c307dcc58eea12e813e6f757
Leigh Halliday <lhalliday@bettertheworld.com>
2014-11-23 15:15:44 -0500

    Setting redis up for heroku

--- config/initializers/idioma.rb
+++ config/initializers/idioma.rb
@@ -1,4 +1,9 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = if Rails.env.development?
+  Redis.new(:host => 'localhost', :port => 6379)
+else
+  Redis.new(:host => ENV["REDISCLOUD_URL"])
+end
+
 I18n.backend = I18n::Backend::KeyValue.new($redis)
 
 Idioma.configure do |configure|
