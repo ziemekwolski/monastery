1126d8a1ce126d4768e085ad91e0d401de32cbc2
Ziemek Wolski <ziemek.wolski@gmail.com>
2015-12-03 22:23:29 -0500

    Fixed issues with migrations because Globalize was removed.

--- db/migrate/20141203015831_add_snippet_to_users.rb
+++ db/migrate/20141203015831_add_snippet_to_users.rb
@@ -1,15 +1,17 @@
 class AddSnippetToUsers < ActiveRecord::Migration
   def up
     add_column :users, :snippet, :text
-    User.create_translation_table!({
-      snippet: :text
-    }, {
-      migrate_data: true
-    })
+    if User.respond_to?("create_translation_table!")
+      User.create_translation_table!({
+        snippet: :text
+      }, {
+        migrate_data: true
+      })
+    end
   end
 
   def down
     remove_column :users, :snippet
-    User.drop_translation_table! migrate_data: true
+    User.drop_translation_table! migrate_data: true if User.respond_to?("drop_translation_table!")
   end
 end
